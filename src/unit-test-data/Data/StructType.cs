fc73052534658f38e0806419c40c198490912c8b
pmosk <p@moskovoi.ru>
2021-02-01 01:00:48 +0300

    Minimal improvement

--- src/unit-test-data/Data/StructType.cs
+++ src/unit-test-data/Data/StructType.cs
@@ -1,9 +1,15 @@
 ﻿#nullable enable
 
+using static System.FormattableString;
+
 namespace PrimeFuncPack.UnitTest
 {
     public struct StructType
     {
         public string? Text { get; set; }
+
+        public override string? ToString()
+            =>
+            Invariant($"{nameof(StructType)}: '{Text}'");
     }
 }
