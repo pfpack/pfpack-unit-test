cb4a824b51080296ca8270fd2342df774c06994c
Pavel Moskovoy <p@moskovoi.ru>
2021-09-23 17:57:14 +0300

    Update Test.Data.RecordType.fs

--- src/unit-test-data/Data.FSharp.Tests/Tests.DataTest/Test.Data.RecordType.fs
+++ src/unit-test-data/Data.FSharp.Tests/Tests.DataTest/Test.Data.RecordType.fs
@@ -12,7 +12,7 @@ module DataRecordTypeTest =
         TestData.PlusFifteenIdSomeStringNameRecord |> shouldBeDeepEqual expected
 
     [<Fact>]
-    let ``TestData.PlusFifteenIdLowerSomeStringNameRecord should be the id is 15 and the name is the some string``() =
+    let ``TestData.PlusFifteenIdLowerSomeStringNameRecord should be the id is 15 and the name is the lower some string``() =
         let expected = RecordType(Id = TestData.PlusFifteen, Name = TestData.LowerSomeString)
         TestData.PlusFifteenIdLowerSomeStringNameRecord |> shouldBeDeepEqual expected
 
